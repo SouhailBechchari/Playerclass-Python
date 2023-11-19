079cd55d452c8b4810942dddfe489fee1a93df4c
Souhaïl Bechchari <146872818+SouhailBechchari@users.noreply.github.com>
2023-11-19 22:40:17 +0100

    Create classPlayer.py

---
+++ classPlayer.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self,name,age,rank):
+        self.name = name
+        self.age = age
+        self.rank = rank
+        
+Player1 = Player('Souhail','18',1 )
+Player2 = Player('Cheb Larbi','34',6 )
+Player3 = Player('bourqadi','28',99 )
